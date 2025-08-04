06487302a1cfcfd5d820470ea461b4fbbcfb790e
Gabriel Lopez Lugo <glugo3@gatech.edu>
2025-08-04 03:17:02 -0400

    Implemented customizable grid and toy UI

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,29 +1,76 @@
-const drawGrid = () => {
+// Constants
+const MAX_LENGTH = 100;
+const MIN_LENGTH = 10;
+
+// Grid methods
+function drawGrid(sideLength = 16) {
     const container = document.querySelector(".container");
     
-    container.style.display = "flex";
-    container.style.justifyContent = "center";
-    container.style.flexWrap = "wrap";
-
-    const NUM_DIVS = 16;
-    for (let i = 0; i < NUM_DIVS; i++) {
-        container.appendChild(createSquare());
+    for (let i = 0; i < sideLength; i++) {
+        container.appendChild(createRow(sideLength));
     } 
 }
 
-const createSquare = () => {
+function resetGrid() {
+    const container = document.querySelector(".container");
+
+    let child = container.lastChild;
+    while (child) {
+        container.removeChild(child);
+        child = container.lastChild;
+    }
+}
+
+function createNewGrid() {
+    resetGrid();
+    const sideLength = promptLength();
+    drawGrid(sideLength);
+}
+
+function createRow(sideLength) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    row.style.display = "flex";
+
+    for (let i = 0; i < sideLength; i++) {
+        row.appendChild(createSquare(sideLength));
+    }
+
+    return row;
+}
+
+function createSquare(sideLength) {
+    // Height is used instead so that it can fit vertically
+    const calculateWidth = sideLength => (100 / sideLength) + "%";
+    
     const square = document.createElement("div");
     square.classList.add("square");
 
-    // Style
-    square.style.width = "24vw";
-    square.style.height = "24vw";
 
-    // Events
-    square.onmouseenter = () => square.style.backgroundColor = "grey";
-    square.onmouseleave = () => square.style.backgroundColor = "white";
-    
+    const width = calculateWidth(sideLength);
+    square.style.backgroundColor = "rgb(201, 201, 201)";
+    square.style.width = width;
+    square.style.aspectRatio = "1 / 1";
+
+    square.onmouseenter = () => square.style.backgroundColor = "grey";    
+
     return square;
-};
+}
+
+function promptLength() {
+    let sideLength = prompt("Enter the squares per side for the new grid: ");
+
+    if (sideLength > MAX_LENGTH) {
+        sideLength = MAX_LENGTH;
+    } else if (sideLength < MIN_LENGTH) {
+        sideLength = MIN_LENGTH;
+    } 
+    return sideLength;
+}
+
+// Event
+const button = document.querySelector("button");
+button.onclick = () => createNewGrid();
 
 drawGrid();
